42a93d9a72e697ca26afac350c6ec286814c2bd4
Steve Bauman <steven_bauman@outlook.com>
2022-01-23 14:19:15 -0500

    WIP

--- src/renderer/components/Label.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <label class="mb-1 text-xs font-semibold text-ui-gray-400">
-        <slot />
-    </label>
-</template>
--- src/renderer/plugins/vue-tailwind.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import VueTailwind from 'vue-tailwind';
-import { TToggle, TModal, TDropdown, TAlert } from 'vue-tailwind/dist/components';
-
-Vue.use(VueTailwind, { TAlert, TToggle, TModal, TDropdown });
