79c7f6efbb8431962c24d8fa8018e294f84bea54
Steve Bauman <steven_bauman@outlook.com>
2022-01-28 10:03:39 -0500

    Fix emoji keyboard on all platforms

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,30 @@
-import { app } from 'electron'
+import { app, globalShortcut } from "electron";
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== "darwin") app.quit();
+});
 
-require('./mainWindow')
+app.whenReady().then(() => {
+  if (!app.isEmojiPanelSupported()) {
+    return;
+  }
+
+  if (process.platform === "darwin") {
+    globalShortcut.register("CommandOrControl+Shift+Space", () =>
+      app.showEmojiPanel()
+    );
+  }
+
+  if (process.platform === "linux") {
+    globalShortcut.register("Control+.", () => app.showEmojiPanel());
+  }
+
+  if (process.platform === "win32") {
+    globalShortcut.register("MetaOrSuper+.", () => app.showEmojiPanel());
+  }
+});
+
+require("./mainWindow");
